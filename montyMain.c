147eedd928c985260917774ffc1978477b01e9fc
bashar-karoury <basharalkaroury@hotmail.com>
2023-12-13 16:12:33 +0200

    Parse lines into words

--- montyMain.c
+++ montyMain.c
@@ -4,6 +4,13 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include "strings.h"
+char **av;
+void free_av(void);
+int parse_line(char *line);
+void fill_zero(char *s, int i);
+void end_with_null(char *s);
 /**
  * main - Entry point of monty program
  * @argc: count of arguments
@@ -15,6 +22,8 @@ int main(int argc, char **argv)
 {
 	FILE *fstream;
 	char *line = NULL;
+/*	int result = 0; */
+	int i = 0;
 	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
@@ -27,17 +36,80 @@ int main(int argc, char **argv)
 		exit(EXIT_FAILURE);
 	}
 
-	line = malloc(sizeof(char) * 100);
+	line = malloc(sizeof(char) * 1000);
 	if (line == NULL)
 	{
 		fprintf(stderr, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);	
 	}
-	while(fgets(line, 100, fstream))
-	{	
-		printf("line : %s", line);	
+	fill_zero(line, 1000);
+	av = malloc(sizeof(char *) * 10);
+
+	if (av == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		free(line);
+		exit(EXIT_FAILURE);
 	}
-	printf("End of file reached\n");
+	fill_zero((char *)av, 10);
+	while(fgets(line, 1000, fstream))
+	{	
+		end_with_null(line);
+		printf("line : %s\n", line);
+		if(parse_line(line) != 0)
+		{
+			for (i = 0; av[i] != NULL; i++)
+				printf("av[%d] = %s\n", i , av[i]);
+		}
+		free_av();
+	}
+	printf("DEBUG: EOF\n");
 	free(line);
+	free(av);
+	fclose(fstream);
 	return(0);
+}
+
+/**
+ * parse_line - tokenize words separated by spaces
+ * @line: string to be tokenized
+ * 
+ * Return: Number of words
+ */
+int	parse_line(char *line)
+{
+	char *token = NULL;
+	int i = 0;
+	token = strtok(line, " ");
+	while(token)
+	{
+		av[i] = _strdup(token);
+		token = strtok(NULL, " ");
+		i++;
+	}
+	av[i] = NULL;
+	return (i);
+}
+
+
+void free_av(void)
+{
+	int i = 0;
+	while (av[i] != NULL)
+		free(av[i++]);
+}
+
+void fill_zero(char *s, int n)
+{
+	int i;
+	for (i = 0; i < n; i++)
+		s[i] = '\0';
+}
+
+void end_with_null(char *s)
+{
+	while(*s != '\0')
+		s++;
+	if (*(--s) == '\n')
+		*(s) = '\0';
 }
